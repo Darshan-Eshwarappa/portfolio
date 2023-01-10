3ed24cf62fe36eb9875a25b03704e5e1b6c8b4d6
Darshan Eshwarappa <mail4dhe@gmail.com>
2023-01-10 18:51:29 +0530

    initial release

---
+++ src/components/qualification/Qualification.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import './qualification.css'
+
+const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index: React.SetStateAction<number>) => {
+        setToggleState(index);
+    }
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My personal journey</span>
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
+                    <i className="uil uil-graduation-cap qualification__icon"></i>Experience
+                </div>
+
+                <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
+                    <i className="uil uil-briefcase-alt qualification__icon"></i>Education
+                </div>
+            </div>
+            <div className="qualification__sections">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                        <h3 className="qualification__title">Data Scientist</h3>
+                        <span className="qualification__subtitle">Longevity Intime - Delaware, U.S</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> 2020 - Present
+                        </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                            <div>
+                            <h3 className="qualification__title">ML Research Engineer</h3>
+                            <span className="qualification__subtitle">Ivy - London, U.K</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2019 - 2020
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Software Developer</h3>
+                            <span className="qualification__subtitle">Rakuten - Bengaluru, India</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2019 
+                            </div>
+                        </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                    </div>
+                     <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                            <div>
+                            <h3 className="qualification__title">Full Stack Development Intern</h3>
+                            <span className="qualification__subtitle">JP Morgan Chase & Co. - Bengaluru, India</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2018 - 2019
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Computer Science & Engineering (B.E)</h3>
+                            <span className="qualification__subtitle">Dayananda Sagar Institution</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2015 - 2019
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                        <h3 className="qualification__title">Pre-University</h3>
+                        <span className="qualification__subtitle">Sri Aurobindo PU College</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> 2014 - 2015
+                        </div>
+                        </div>
+                    </div>
+                 <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">High School</h3>
+                        <span className="qualification__subtitle">Highway English School</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i> 2013 - 2014
+                        </div>
+                    </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification
---
+++ src/components/testimonials/Data.tsx
@@ -0,0 +1,27 @@
+import Image1 from "../../assets/daniel.jpg";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Daniel Lenton",
+    description:
+      "A really good job, all aspects of the projects were followed step by step and with good results.",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Allie Sheffield",
+    description:
+      "Darshan has been an instrumental enginner in the core projects at Longevity Intime. His performance is consistent and on-time",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Sreenivasulu M",
+    description:
+      "An illustrious developer with keen eye on product development and quality of the product",
+  },
+];
